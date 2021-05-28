4fb522daeeb4617f620f94a044d1979e03137cda
fauzanhilmy <fauzanhilmy20@gmail.com>
2021-05-28 15:34:16 +0700

    first commit login screen

--- civika-client/screen/Login.jsx
+++ civika-client/screen/Login.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import { View, Button } from "react-native";
-import { Container, Card, CardItem, Body, Text } from "native-base";
+import { View, Image, Dimensions } from "react-native";
+import { Container, Card, CardItem, Body, Text, Header, Content, Form, Item, Input, Label, Button } from "native-base";
+
+const width = Dimensions.get("window").width-5
 export default function Login({ navigation }) {
   function submitLogin() {
     navigation.navigate("BottomTabHome");
   }
   return (
-    <Container>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Card>
-          <CardItem>
-            <Text>Halaman login</Text>
-            <Button title="Login dulu yak" onPress={submitLogin} />
-          </CardItem>
-        </Card>
-      </View>
+    // <View>
+    <Container style={{backgroundColor: "#f9f7f7",}}>
+        <Content contentContainerStyle={{justifyContent: "center", flex: 1}}>
+        <Image source= {require('../assets/CIVIKA.png')} style={{ height: 400, width: 400, alignSelf: "center", marginTop: -50}}/>
+          <Form style={{marginTop: -60}}>
+            <Item floatingLabel style={{marginLeft: 60, marginRight:60, alignItems: "center"}} >
+              <Label style={{alignSelf: "center", fontSize: 14}}>Email</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last style={{marginLeft: 60, marginRight:60}}>
+              <Label style={{alignSelf: "center", fontSize: 14}}>Kata sandi</Label>
+              <Input />
+            </Item>
+            <Button rounded style={{alignSelf: "center", marginTop: 50, backgroundColor: "#112d4e"}}
+            onPress={submitLogin}
+            >
+            <Text>Masuk</Text>
+          </Button>
+          </Form>
+        </Content>
+
     </Container>
   );
 }
