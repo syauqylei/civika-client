ba716c22a263266caa044deda5cde80312df9bf6
Riski Putra Awali <riskiputra270@gmail.com>
2021-05-29 00:03:12 +0700

    home screen, minus banner

--- civika-client/components/StudentMenu.jsx
+++ civika-client/components/StudentMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View } from "react-native";
+import { Button, Text } from "native-base";
 import Ionicon from "react-native-vector-icons/Ionicons";
 
 export default function StudentMenu({ navigation }) {
@@ -7,97 +8,57 @@ export default function StudentMenu({ navigation }) {
     navigation.navigate(screen);
   }
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, flexDirection: "row" }}>
       <View style={{ flex: 1 }}>
-        <TouchableOpacity
+        <Button
+          Icon
+          large
+          style={{
+            alignSelf: "center",
+            backgroundColor: "#112d4e",
+            marginVertical: 15,
+            width: 60,
+            justifyContent: "center",
+          }}
           onPress={() => changeScreen("KRS")}
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            height: 70,
-            backgroundColor: "#dbe2ef",
-            borderColor: "#005792",
-            borderWidth: 2,
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            borderTopRightRadius: 10,
-            borderTopLeftRadius: 10,
-            marginHorizontal: 6,
-            marginVertical: 6,
-          }}
         >
-          <Ionicon name="newspaper-outline" size={40} color="#005792" />
-          <Text
-            style={{
-              flex: 0.78,
-              color: "#005792",
-              alignSelf: "center",
-              textAlign: "center",
-              fontSize: 20,
-            }}
-          >
-            Kartu Rencana Studi
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
+          <Ionicon name="card" size={30} color="#dbe2ef" />
+        </Button>
+        <Text style={{ alignSelf: "center" }}>KRS</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Button
+          large
+          Icon
+          style={{
+            alignSelf: "center",
+            backgroundColor: "#112d4e",
+            marginVertical: 15,
+            width: 60,
+            justifyContent: "center",
+          }}
           onPress={() => changeScreen("UKT")}
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            height: 40,
-            backgroundColor: "#dbe2ef",
-            borderColor: "#c70039",
-            borderWidth: 2,
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            marginHorizontal: 6,
-            marginVertical: 6,
-          }}
         >
-          <Ionicon name="ios-wallet-outline" size={40} color="#c70039" />
-          <Text
-            style={{
-              flex: 0.78,
-              color: "#c70039",
-              flexWrap: "wrap",
-              alignSelf: "center",
-              textAlign: "center",
-              fontSize: 20,
-            }}
-          >
-            Uang Kuliah Tunggal
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
+          <Ionicon name="wallet" size={30} color="#dbe2ef" />
+        </Button>
+        <Text style={{ alignSelf: "center" }}>UKT</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Button
+          large
+          Icon
+          style={{
+            alignSelf: "center",
+            backgroundColor: "#112d4e",
+            marginVertical: 15,
+            width: 60,
+            justifyContent: "center",
+          }}
           onPress={() => changeScreen("SKS")}
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            backgroundColor: "#dbe2ef",
-            borderColor: "#00818a",
-            borderWidth: 2,
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            borderBottomLeftRadius: 10,
-            borderBottomRightRadius: 10,
-            marginHorizontal: 6,
-            marginVertical: 6,
-          }}
         >
-          <Ionicon name="ios-school-outline" size={40} color="#00818a" />
-          <Text
-            style={{
-              flex: 0.78,
-              color: "#00818a",
-              flexWrap: "wrap",
-              alignSelf: "center",
-              textAlign: "center",
-              fontSize: 20,
-            }}
-          >
-            Satuan Kredit Semester
-          </Text>
-        </TouchableOpacity>
+          <Ionicon name="school" size={30} color="#dbe2ef" />
+        </Button>
+        <Text style={{ alignSelf: "center" }}>SKS</Text>
       </View>
     </View>
   );
