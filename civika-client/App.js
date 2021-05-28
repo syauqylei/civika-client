86b15c4fb407281869dfb89b5916211d62244674
Riski Putra Awali <riskiputra270@gmail.com>
2021-05-29 01:21:17 +0700

    done login

--- civika-client/App.js
+++ civika-client/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginStackNavigator from "./navigator/LoginStackNavigator";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    }).then((_) => {
+      setIsReady(true);
+    });
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator color="black" size="large" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <LoginStackNavigator />
--- civika-client/screen/Login.jsx
+++ civika-client/screen/Login.jsx
@@ -1,34 +1,97 @@
-import React from "react";
-import { View, Image, Dimensions } from "react-native";
-import { Container, Card, CardItem, Body, Text, Header, Content, Form, Item, Input, Label, Button } from "native-base";
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  ScrollView,
+  Platform,
+  StatusBar,
+  KeyboardAw,
+} from "react-native";
+import {
+  Container,
+  Text,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+} from "native-base";
 
-const width = Dimensions.get("window").width-5
 export default function Login({ navigation }) {
+  const [dataLogin, setDataLogin] = useState({ email: "", password: "" });
+
   function submitLogin() {
     navigation.navigate("BottomTabHome");
   }
+
   return (
-    // <View>
-    <Container style={{backgroundColor: "#f9f7f7",}}>
-        <Content contentContainerStyle={{justifyContent: "center", flex: 1}}>
-        <Image source= {require('../assets/CIVIKA.png')} style={{ height: 400, width: 400, alignSelf: "center", marginTop: -50}}/>
-          <Form style={{marginTop: -60}}>
-            <Item floatingLabel style={{marginLeft: 60, marginRight:60, alignItems: "center"}} >
-              <Label style={{alignSelf: "center", fontSize: 14}}>Email</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel last style={{marginLeft: 60, marginRight:60}}>
-              <Label style={{alignSelf: "center", fontSize: 14}}>Kata sandi</Label>
-              <Input />
-            </Item>
-            <Button rounded style={{alignSelf: "center", marginTop: 50, backgroundColor: "#112d4e"}}
-            onPress={submitLogin}
+    <ScrollView
+      style={{
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : null,
+      }}
+    >
+      <Container style={{ backgroundColor: "#f9f7f7" }}>
+        <Content contentContainerStyle={{ justifyContent: "center", flex: 1 }}>
+          <Image
+            source={require("../assets/CIVIKA.png")}
+            style={{
+              height: 400,
+              width: 400,
+              alignSelf: "center",
+              marginTop: -50,
+            }}
+          />
+          <Form style={{ marginTop: -60 }}>
+            <Item
+              floatingLabel
+              style={{ marginLeft: 60, marginRight: 60, alignItems: "center" }}
             >
-            <Text>Masuk</Text>
-          </Button>
+              <Label style={{ alignSelf: "center", fontSize: 14 }}>Email</Label>
+              <Input
+                value={dataLogin.email}
+                onChangeText={(inputEmail) =>
+                  setDataLogin((state) => ({
+                    ...state,
+                    email: inputEmail,
+                  }))
+                }
+                style={{ textAlign: "center" }}
+              />
+            </Item>
+            <Item
+              floatingLabel
+              last
+              style={{ marginLeft: 60, marginRight: 60 }}
+            >
+              <Label style={{ alignSelf: "center", fontSize: 14 }}>
+                Kata sandi
+              </Label>
+              <Input
+                value={dataLogin.password}
+                onChangeText={(inputPassword) =>
+                  setDataLogin((state) => ({
+                    ...state,
+                    password: inputPassword,
+                  }))
+                }
+                secureTextEntry={true}
+                style={{ textAlign: "center" }}
+              />
+            </Item>
+            <Button
+              rounded
+              style={{
+                alignSelf: "center",
+                marginTop: 50,
+                backgroundColor: "#112d4e",
+              }}
+              onPress={submitLogin}
+            >
+              <Text>Masuk</Text>
+            </Button>
           </Form>
         </Content>
-
-    </Container>
+      </Container>
+    </ScrollView>
   );
 }
